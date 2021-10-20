76a7719c04bdc168c57aea309d122a10c1dda9e3
KojimaMcMaple <KyleHunter.KH@outlook.com>
2021-10-20 00:14:42 -0400

    reorient enemy

--- GAME2014-F2021-MidTerm-101264698/Assets/[Scripts]/EnemyController.cs
+++ GAME2014-F2021-MidTerm-101264698/Assets/[Scripts]/EnemyController.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float horizontalSpeed;
-    public float horizontalBoundary;
-    public float direction;
+    public float moveSpeed = 2.0f;
+    public float moveBoundary = 2.1f;
+    public float direction = 1.0f;
+
+    void Awake()
+    {
+        Bounds sprite_bounds = GetComponent<SpriteRenderer>().sprite.bounds;
+        Vector3 top_right_max_pos = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
+        moveBoundary = (Mathf.Abs(top_right_max_pos.y) - (sprite_bounds.extents.y * transform.localScale.y)); //find the screen bounds in world, then substract the sprite size to set bounds
+    }
 
     // Update is called once per frame
     void Update()
@@ -17,19 +24,19 @@ public class EnemyController : MonoBehaviour
 
     private void _Move()
     {
-        transform.position += new Vector3(horizontalSpeed * direction * Time.deltaTime, 0.0f, 0.0f);
+        transform.position += new Vector3(0.0f, moveSpeed * direction * Time.deltaTime, 0.0f);
     }
 
     private void _CheckBounds()
     {
         // check right boundary
-        if (transform.position.x >= horizontalBoundary)
+        if (transform.position.y >= moveBoundary)
         {
             direction = -1.0f;
         }
 
         // check left boundary
-        if (transform.position.x <= -horizontalBoundary)
+        if (transform.position.y <= -moveBoundary)
         {
             direction = 1.0f;
         }
